ecf0225def6920e6fa8bbb748c2f5f179270efe1
Hannah <vining.h@husky.neu.edu>
2024-04-03 17:47:51 -0400

    first upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import CourseRoutes from "./Kanbas/courses/routes.js";
+import ModuleRoutes from "./Kanbas/modules/routes.js";
+import AssignmentRoutes from './Kanbas/assignments/routes.js';
+import cors from "cors";
+const app = express();
+app.use(cors());
+app.use(express.json());
+AssignmentRoutes(app);
+ModuleRoutes(app);
+CourseRoutes(app);
+app.listen(process.env.PORT || 4000);
